6217265119df143a525928b2463ea3b199d023bc
vmurali100 <vmurali100@users.noreply.github.com>
2021-09-29 18:05:41 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var abc="Hello World"
   res.render('index', { title: 'Express Route Application' });
 });
 
