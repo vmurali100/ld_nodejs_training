181ef2230a1451eed36c324064313acaa349eedc
vmurali100 <vmurali1100@gmail.com>
2021-09-29 18:19:36 +0530

    Changes in Route file

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
   var abc="Hello World"
-  //res.render('index', { title: 'Express Route Application' });
-
   res.render('index', { title: 'Express Route Application from Master Branch' });
 
 });
